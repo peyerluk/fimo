50b70be9cdea7bb2b565e40799baae19875e0756
Lukas <lpeyer@gmail.com>
2012-07-12 19:18:59 +0200

    moved browser user logic into its on file

--- public/coffee/fimo.coffee
+++ public/coffee/fimo.coffee
@@ -7,29 +7,10 @@ fimo = @fimo
 fimo.init = ->
   controller = fimo.controller
   
-  # authentication
-  create_user = () ->
-    # create user
-    # TODO: add in coordinates
-    pretty_unique_name = "#{Math.random().toString(32)}"
-    fimo.data.post 'register', {email:"#{pretty_unique_name}@fimo.com", username:"#{pretty_unique_name}", password:"notsosecure"}, ->
-      # login user
-      fimo.data.post 'login', {email:"#{pretty_unique_name}@fimo.com", password:'notsosecure'}, ->
-        # store user to local store
-        store.set('user', {email:"#{pretty_unique_name}@fimo.com", password:'notsosecure'})
+  # init user
+  fimo.user.init()
 
-  user = store.get('user')
-  if user
-    # login user
-    console.log('logging in existing user')
-    fimo.data.post 'login', {'email':user.email, 'password': user.password}, undefined, ->
-      create_user()
-  else
-    console.log('about to create user')
-    create_user()
-
-  
-  # navigation
+  # navigation events
   $(document).on "click", "a", (event) ->
     event.preventDefault()
     
@@ -42,6 +23,7 @@ fimo.init = ->
   
     undefined
   
+  # clicks on object images
   imageIdRegex = /\w*(?=_\w*\.(?=jpg|png))/i
   $(document).on "click", (event) ->
     imageSrc = event.target.getAttribute("src")
--- public/js/app/user.js
+++ public/js/app/user.js
@@ -1,5 +1,47 @@
 (function() {
 
-  this.fimo.user = (function() {})();
+  this.fimo.user = (function() {
+    var create_user;
+    create_user = function() {
+      var pretty_unique_name;
+      pretty_unique_name = "" + (Math.random().toString(32));
+      return fimo.data.post('register', {
+        email: "" + pretty_unique_name + "@fimo.com",
+        username: "" + pretty_unique_name,
+        password: "notsosecure"
+      }, function() {
+        return fimo.data.post('login', {
+          email: "" + pretty_unique_name + "@fimo.com",
+          password: 'notsosecure'
+        }, function() {
+          return store.set('user', {
+            email: "" + pretty_unique_name + "@fimo.com",
+            password: 'notsosecure'
+          });
+        });
+      });
+    };
+    return {
+      /* PUBLIC
+      */
+
+      init: function() {
+        var user;
+        user = store.get('user');
+        if (user) {
+          console.log('logging in existing user');
+          return fimo.data.post('login', {
+            'email': user.email,
+            'password': user.password
+          }, void 0, function() {
+            return create_user();
+          });
+        } else {
+          console.log('about to create user');
+          return create_user();
+        }
+      }
+    };
+  })();
 
 }).call(this);
