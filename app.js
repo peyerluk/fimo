d5d21d2f706f90fd9c3402ff24eb2b9175cc96ca
Lukas <lpeyer@gmail.com>
2012-07-25 14:38:18 +0200

    server starts again

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var app = require("./app/controllers/app"),
     users = require("./app/controllers/users"),
-    images = require("./app/controllers/images"),
     objects = require("./app/controllers/objects"),
     jumbles = require("./app/controllers/jumbles"),
     javascript = require("./app/config/javascript");
